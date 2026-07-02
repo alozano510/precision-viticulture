6b555799908a5edcb36b82e1e64b1f3f7a12ce3b
Andres <alozano510@gmail.com>
2026-07-02 20:23:32 +0000

    changed to use ultralytics export method

--- utils/coversion/yolo_onnx_to_rknn.py
+++ utils/coversion/yolo_onnx_to_rknn.py
@@ -1,35 +1,3 @@
-"""
-Converts and exports a YOLO models in ONNX framework as a Rockchip Neural Network (RKNN)
-IMPORTANT: this program only works on x86 Linux systems.
-"""
-import os
-import argparse
-from rknn.api import RKNN
-
-parser = argparse.ArgumentParser()
-parser.add_argument('model_path', type=str)
-args = parser.parse_args()
-
-model = args.model_path
-
-while not model:
-    print(f"Specify the models path for the ONNX to convert")
-    model = input("Enter models path: ").strip()
-
-# Convert ONNX to RKNN
-rknn = RKNN()
-
-rknn.config(
-    mean_values=[[0, 0, 0]],
-    std_values=[[255, 255, 255]],
-    target_platform='rk3588',
-    output_optimize=True
-)
-
-rknn.load_onnx(model=model)
-rknn.build(do_quantization=False)
-model_name = os.path.splitext(os.path.basename(model))[0]
-rknn.export_rknn(model_name + ".rknn")
-
-print("Exported ONNX models to RKNN")
-rknn.release()
+from ultralytics import YOLO
+model = YOLO("utils/coversion/best.pt")
+model.export(format="rknn", name="rk3588")
